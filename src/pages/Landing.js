8fafce6d193679e8dd266356980eac30dd2aed34
Vanan T <2146063@cognizant.com>
2024-02-17 13:38:48 +0530

    first commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,31 @@
+import React from "react";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>Tracking</span> app
+          </h1>
+          <p>
+            Keep track of all your job applications and manage your job search
+            all in one place.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job-hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
